72f5a2a06db3096b9fc40041686fd105d2163c3a
Raghu Varma <raghuvarma@masymbol.com>
2013-12-20 19:24:58 +0530

    add admin access for delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 SampleApp::Application.routes.draw do
+  devise_for :admins
+
   get "welcome/index"
 
-  devise_for :users
+  devise_for :users, path_names:{sign_in:"login", sign_out: "logout"}
 
   resources :posts
 
