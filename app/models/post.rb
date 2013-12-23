b76de31bcfced97f7bf1cbd7a1e04b3437ad828a
Raghu Varma <raghuvarma@masymbol.com>
2013-12-23 14:44:15 +0530

    add carrierwave gem for file uploading.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name
+  #require 'carrierwave/orm/activerecord'
+  attr_accessible :content, :name, :image
+  mount_uploader :image, ImageUploader
 end
