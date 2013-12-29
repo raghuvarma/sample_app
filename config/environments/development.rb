00abd85f27406316a49545a1cd3e159bf9a35b52
Raghu Varma <raghuvarma@masymbol.com>
2013-12-29 21:37:32 +0530

    add mailers configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,15 @@ SampleApp::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'www.google.com',
+    :user_name            => 'rrv461@gmail.com',
+    :password             => '185555110',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+    }
+  
 end
